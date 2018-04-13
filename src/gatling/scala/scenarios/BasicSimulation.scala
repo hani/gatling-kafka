d242e9e4f09cdcbdf029d481f3666dbad4d2fe90
Hani Suleiman <hani@formicary.net>
2018-04-13 10:54:24 -0400

    Sample scenario and reduce logging to info

--- src/gatling/scala/scenarios/BasicSimulation.scala
+++ src/gatling/scala/scenarios/BasicSimulation.scala
@@ -9,20 +9,20 @@ import scala.concurrent.duration._
 class BasicSimulation extends Simulation {
   val protocol = KafkaProtocol(Map(
     ProducerConfig.ACKS_CONFIG -> "1",
-    ProducerConfig.BOOTSTRAP_SERVERS_CONFIG -> "localhost:9092",
+    ProducerConfig.BOOTSTRAP_SERVERS_CONFIG -> "localhost:19092",
 
     ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG ->
-            "org.apache.kafka.common.serialization.StringSerializer",
+            classOf[org.apache.kafka.common.serialization.LongSerializer].getName,
     ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG ->
-            "org.apache.kafka.common.serialization.StringSerializer"))
+            classOf[org.apache.kafka.common.serialization.StringSerializer].getName))
 
   val scn = scenario("Kafka Test")
           .exec(
             kafka("request").send
-                    .topic("mytopic")
-                    .message[String, String]("foo", "bar")
+                    .topic("loadtest")
+                    .message[Long, String](System.currentTimeMillis(), System.currentTimeMillis() + "text")
           )
 
-  setUp(scn.inject(constantUsersPerSec(10) during (90 seconds)))
+  setUp(scn.inject(constantUsersPerSec(1000) during (60 seconds)))
           .protocols(protocol)
 }
